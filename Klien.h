2733e519fc32baf47adbfd214dd3a13f031c99a1
Nama Kamu <emailkamu@example.com>
2025-11-27 22:43:19 +0700

    Menambah ADT child klien.h untuk proggses minggu ke 11

--- Klien.h
+++ Klien.h
@@ -1,8 +1,22 @@
 #ifndef KLIEN_H_INCLUDED
 #define KLIEN_H_INCLUDED
 #include <iostream>
-
 using namespace std;
 
+struct InfoChild {
+
+};
+
+typedef struct NodeChild *PointerChild;
+
+
+struct NodeChild {
+    InfoChild data;
+    PointerChild next;
+};
+
+struct ListChild {
+    PointerChild first;
+};
 
 #endif // KLIEN_H_INCLUDED
